f69f1866b6722a0175825b051992d0d0d5d93f3c
Tomas Aranda <tomasaranda2018@gmail.com>
2023-11-25 05:04:52 -0300

    Add theme colors to theme.ts

---
+++ src/theme/theme.ts
@@ -0,0 +1,18 @@
+export const theme = {
+    primary: '#020e22',
+    secondary: '#0b2252',
+    lightGrey: '#F8F8F8',
+    mediumGrey: '#E5E5E5',
+    darkGrey: '#6E6E82',
+    white: '#ffffff',
+    green: '#3DAB25',
+    greenDark: '#006D7A',
+    greenMedium: '#00A184',
+    greenLight: '#3ed18b',
+    red: '#ff3d3d',
+    black: '#232440',
+    cyan: '#2ED7FF',
+    yellow: '#fdd603',
+    blue: '#003B56'
+  };
+  
